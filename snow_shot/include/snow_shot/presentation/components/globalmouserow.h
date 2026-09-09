2dd66857de9fdadfe5319c4d9710a1c15fd702e6
mg-chao <87016106+mg-chao@users.noreply.github.com>
2026-09-10 02:05:58 +0800

    refactor(global-mouse): use unified configuration form (#1113)

--- snow_shot/include/snow_shot/presentation/components/globalmouserow.h
+++ snow_shot/include/snow_shot/presentation/components/globalmouserow.h
@@ -13,6 +13,7 @@ class QLabel;
 class QEvent;
 class QMouseEvent;
 namespace adqt::widgets {
+class AdFormItem;
 class AdModal;
 class AdSelect;
 } // namespace adqt::widgets
@@ -60,8 +61,8 @@ class GlobalMouseRow final : public ActionRow {
     snow_shot::presentation::settings::SettingsGlobalMouseCombination m_combination;
     ShortcutConfigurationButton* m_button = nullptr;
     QPointer<adqt::widgets::AdModal> m_modal;
-    QPointer<QLabel> m_activationField;
-    QPointer<QLabel> m_mouseButtonField;
+    QPointer<adqt::widgets::AdFormItem> m_activationField;
+    QPointer<adqt::widgets::AdFormItem> m_mouseButtonField;
     QPointer<QLabel> m_validationLabel;
     QPointer<adqt::widgets::AdSelect> m_activationSelect;
     QPointer<adqt::widgets::AdSelect> m_mouseButtonSelect;
